815766512080a189de8e3b3115bfdf89c2251283
Evan Chernicky <echernicky@gmail.com>
2022-05-20 00:14:43 -0400

    fixed background

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,18 @@ import React from 'react'
 import foam from '../assets/foam.svg'
 
 function Header() {
+
+//<img className="absolute h-screen w-100 inset-0 m-auto" src={foam} alt="background" />
+
   return (
-    <header className="App-header h-screen flex flex-col justify-center items-center" style={{backgroundImage: `url('${foam})`}}>
+    <header className="App-header h-screen flex flex-col justify-center items-center w-100 bg-no-repeat	bg-cover bg-center" style={{backgroundImage: `url(${foam})`}}>
         <div className="z-10 flex flex-col text-center text-white">
             <h1 className="text-2xl m-auto mb-6" style={{maxWidth: '60%'}}>
                 Hello, my name is Evan Chernicky.<br/>
                 I am a Washington, DC frontend developer with experience in JavaScript, React, Ruby, Ruby on Rails, and more.
             </h1>
-            <a className="border border-white inline-block w-34 p-3 m-auto" href="#skills">LEARN MORE</a>
+            <a className="border border-white inline-block w-34 py-3 px-5 m-auto hover:bg-white hover:text-black duration-500" href="#skills">LEARN MORE</a>
         </div>
-        <img className="absolute h-screen w-100 inset-0 m-auto" src={foam} alt="background" />
     </header>
   )
 }
