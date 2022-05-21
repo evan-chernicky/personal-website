28d1d564921742bacd718db3119d15141f806af7
Evan Chernicky <echernicky@gmail.com>
2022-05-21 09:28:24 -0400

    reorganized a bit

---
+++ src/components/data/SkillsData.js
@@ -0,0 +1,94 @@
+import ReactBlack from '../../assets/skills/black/react-black.svg'
+import ReactWhite from '../../assets/skills/white/react-white.svg'
+import JavascriptBlack from '../../assets/skills/black/javascript-black.svg'
+import JavascriptWhite from '../../assets/skills/white/javascript-white.svg'
+import BootstrapBlack from '../../assets/skills/black/bootstrap-black.svg'
+import BootstrapWhite from '../../assets/skills/white/bootstrap-white.svg'
+import RubyWhite from '../../assets/skills/white/ruby-white.svg'
+import RubyBlack from '../../assets/skills/black/ruby-black.svg'
+import RailsWhite from '../../assets/skills/white/rails-white.svg'
+import RailsBlack from '../../assets/skills/black/rails-black.svg'
+import PHPWhite from '../../assets/skills/white/php-white.svg'
+import PHPBlack from '../../assets/skills/black/php-black.svg'
+import PostgreWhite from '../../assets/skills/white/postgre-white.svg'
+import PostgreBlack from '../../assets/skills/black/postgre-black.svg'
+import WordpressWhite from '../../assets/skills/white/wordpress-white.svg'
+import WordpressBlack from '../../assets/skills/black/wordpress-black.svg'
+import TailwindWhite from '../../assets/skills/white/tailwind-white.svg'
+import TailwindBlack from '../../assets/skills/black/tailwind-black.svg'
+import SASSWhite from '../../assets/skills/white/sass-white.svg'
+import SASSBlack from '../../assets/skills/black/sass-black.svg'
+import HTMLWhite from '../../assets/skills/white/html-white.svg'
+import HTMLBlack from '../../assets/skills/black/html-black.svg'
+import CSSWhite from '../../assets/skills/white/css-white.svg'
+import CSSBlack from '../../assets/skills/black/css-black.svg'
+import XDWhite from '../../assets/skills/white/xd-white.svg'
+import XDBlack from '../../assets/skills/black/xd-black.svg'
+
+export const skills = [
+    {
+        name: "JavaScript",
+        imageWhite: JavascriptWhite,
+        imageBlack: JavascriptBlack
+    },
+    {
+        name: "React.js",
+        imageWhite: ReactWhite,
+        imageBlack: ReactBlack
+    },
+    {
+        name: "Ruby",
+        imageWhite: RubyWhite,
+        imageBlack: RubyBlack
+    },
+    {
+        name: "Rails",
+        imageWhite: RailsWhite,
+        imageBlack: RailsBlack
+    },
+    {
+        name: "PHP",
+        imageWhite: PHPWhite,
+        imageBlack: PHPBlack
+    },
+    {
+        name: "PostgreSQL",
+        imageWhite: PostgreWhite,
+        imageBlack: PostgreBlack
+    },
+    {
+        name: "WordPress",
+        imageWhite: WordpressWhite,
+        imageBlack: WordpressBlack
+    },
+    {
+        name: "Tailwind",
+        imageWhite: TailwindWhite,
+        imageBlack: TailwindBlack
+    },
+    {
+        name: "SASS",
+        imageWhite: SASSWhite,
+        imageBlack: SASSBlack
+    },
+    {
+        name: "Bootstrap",
+        imageWhite: BootstrapWhite,
+        imageBlack: BootstrapBlack
+    },
+    {
+        name: "HTML",
+        imageWhite: HTMLWhite,
+        imageBlack: HTMLBlack
+    },
+    {
+        name: "CSS",
+        imageWhite: CSSWhite,
+        imageBlack: CSSBlack
+    },
+    {
+        name: "XD",
+        imageWhite: XDWhite,
+        imageBlack: XDBlack
+    },
+]
