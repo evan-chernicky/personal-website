51261fb16b03f66510e6c2a3653127621bfa578d
Evan Chernicky <echernicky@gmail.com>
2022-05-21 21:01:10 -0400

    some changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import emailjs from 'emailjs-com'
 import wave from '../assets/wave-bottom.svg'
 import LightAvatar from '../assets/light-avatar.png'
 
 
-function Contact() {
+function Contact({isDarkMode}) {
   return (
     <div className="min-h-screen w-full bg-no-repeat bg-cover flex items-center" style={{backgroundImage: `url(${wave})`, backgroundPosition: '50% 32vh'}}>
         <div className="max-w-5xl flex m-auto bg-white/90 dark:bg-black/90 p-12 dark:text-white text-black rounded-2xl">
@@ -21,22 +22,25 @@ function Contact() {
                 <input
                     type="text"
                     id="name"
+                    required
                     className="mt-1 mb-4 outline outline-2 border-orange text-left bg-transparent dark:text-white px-2 focus:outline-pink"
                 />
                 <label htmlFor="email" className="dark:text-white font-medium text-left pb-2">Email:</label>
                 <input
                     type="email"
                     id="email"
+                    required
                     className="mt-1 mb-4 outline outline-2  border-orange text-left bg-transparent dark:text-white px-2 focus:outline-pink"
                 />
                 <label htmlFor="message" className="dark:text-white font-medium text-left pb-2">Message:</label>
                 <textarea
                     type="text"
+                    required
                     id="message"
                     className="mt-1 mb-4  outline outline-2 border-orange text-left bg-transparent dark:text-white px-2 focus:outline-pink"
                     style={{minHeight: "100px"}}
                 />
-                  <input className="text-left cursor-pointer" type="submit" value="Submit"/>
+                  <input className="outline hover:text-pink dark:hover:text-blue outline-1 outline-black dark:outline-white dark:hover:outline-blue hover:outline-pink text-left cursor-pointer  max-w-min py-2 px-6" type="submit" value="Submit"/>
             </form>
         </div>
     </div>
