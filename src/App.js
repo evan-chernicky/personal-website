ed5342cdb53839fb54679730a98016c6834a1541
Evan Chernicky <echernicky@gmail.com>
2022-06-03 11:50:42 -0400

    added react router and about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, {useEffect, useState} from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
 import './App.css';
-import Intro from './components/Intro.js'
-import Header from './components/Header.js'
-import Skills from './components/Skills.js'
-import Projects from './components/Projects.js'
-import Contact from './components/Contact.js'
-import DownArrowWhite from './assets/down-arrow-white.svg'
 
 
 function App() {
@@ -63,25 +60,10 @@ function App() {
 
   return (
     <div  id="site" className={`App ${isDarkMode ? 'dark' : 'light'}`}>
-      <div className="bg-white dark:bg-black">
-          <Header setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode}/>
-          <section id="home" className="pb-10 relative">
-            <Intro />
-            <a className="flex flex-col items-center justify-center absolute bottom-16 right-0 left-0 m-auto z-20 max-w-min" href="#skills">
-              <span className="text-white hover:text-blue dark:text-white mb-2 text-xl dark:hover:text-blue">Skills</span>
-              <img className="w-3 animate-bounce" src={DownArrowWhite} alt="Go to skills" />
-            </a>
-          </section>
-          <section id="skills" className="my-24 relative">
-            <Skills isDarkMode={isDarkMode}/>
-          </section>
-          <section id="projects" className="py-10 relative">
-            <Projects isDarkMode={isDarkMode}/>
-          </section>
-          <section id="contact" className="relative">
-            <Contact isDarkMode={isDarkMode}/>
-          </section>
-      </div>
+    <Routes>
+      <Route path="*" exact element={<Home isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+      <Route path="/about" exact element={<About isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />} />
+    </Routes>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react'
+import Intro from './Intro.js'
+import Header from './Header.js'
+import Skills from './Skills.js'
+import Projects from './Projects.js'
+import Contact from './Contact.js'
+import DownArrowWhite from '../assets/down-arrow-white.svg'
+
+
+function App({isDarkMode, setIsDarkMode}) {
+
+  return (
+    <div  id="site" className={`App ${isDarkMode ? 'dark' : 'light'}`}>
+      <div className="bg-white dark:bg-black">
+          <Header setIsDarkMode={setIsDarkMode} isDarkMode={isDarkMode}/>
+          <section id="home" className="pb-10 relative">
+            <Intro />
+            <a className="flex flex-col items-center justify-center absolute bottom-16 right-0 left-0 m-auto z-20 max-w-min" href="#skills">
+              <span className="text-white hover:text-blue dark:text-white mb-2 text-xl dark:hover:text-blue">Skills</span>
+              <img className="w-3 animate-bounce" src={DownArrowWhite} alt="Go to skills" />
+            </a>
+          </section>
+          <section id="skills" className="my-24 relative">
+            <Skills isDarkMode={isDarkMode}/>
+          </section>
+          <section id="projects" className="py-10 relative">
+            <Projects isDarkMode={isDarkMode}/>
+          </section>
+          <section id="contact" className="relative">
+            <Contact isDarkMode={isDarkMode}/>
+          </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
