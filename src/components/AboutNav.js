947945ba870dd15bcb870661eb45a67628aa63d7
evan-chernicky <echernicky@gmail.com>
2023-04-25 15:27:03 -0400

    removed unused imports

--- src/components/AboutNav.js
+++ src/components/AboutNav.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useEffect} from 'react'
 
 function AboutNav({showMobileNav, setShowMobileNav, width}) {
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import Intro from './Intro.js'
 import Header from './Header.js'
 import Skills from './Skills.js'
