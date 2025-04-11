e032daba890eb9694a24c33597d4c18bd5e76183
evan-chernicky <echernicky@gmail.com>
2025-04-11 14:33:50 +0100

    slight adjustment with filtering classes to make it work on light mode

--- src/components/SingleFilter.js
+++ src/components/SingleFilter.js
@@ -2,10 +2,10 @@
 import React from 'react'
 
 function SingleFilter({projectType, currentFilter, setCurrentFilter}) {
-    const activeClasses = (projectType === currentFilter) ? 'dark:border-white border-black' : ''
+    const activeClasses = (projectType === currentFilter) ? 'dark:border-white border-black' : 'border-transparent'
 
   return (
-    <button onClick={() => setCurrentFilter(projectType)} className={`${activeClasses} border border-transparent py-2 px-4 rounded-xl`}>{projectType}</button>
+    <button onClick={() => setCurrentFilter(projectType)} className={`border py-2 px-4 rounded-xl ${activeClasses}`}>{projectType}</button>
   )
 }
 
